66e489cbd988ac57e42ff3165edc61a4ad171cd8
zenakrua <zenakrua@msn.com>
2019-02-21 16:51:56 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,20 @@
-
+<script>
+  var obj, dbParam, xmlhttp, myObj, x, txt = "";
+  obj = { table: "customers" };
+  dbParam = JSON.stringify(obj);
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myObj = JSON.parse(this.responseText);
+      txt += "<table border='1'>"
+      for (x in myObj) {
+        txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+      }
+      txt += "</table>"    
+      document.getElementById("demo").innerHTML = txt;
+    }
+  };
+  xmlhttp.open("POST", "scripts/characters.js", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send("x=" + dbParam);
+</script>
